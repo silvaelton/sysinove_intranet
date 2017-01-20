9086cb70fc12589dd9374e1d865b056270c62825
elton silva <elton.chrls@gmail.com>
2017-01-20 15:53:17 -0200

    fix css

--- app/assets/javascripts/custom-application.js
+++ app/assets/javascripts/custom-application.js
@@ -16,11 +16,13 @@ $(function() {
     $(this).closest('fieldset').hide();
     return event.preventDefault();
   });
-  return $('form').on('click', '.add_fields', function(event) {
+  
+  $('form').on('click', '.add_fields', function(event) {
     var regexp, time;
     time = new Date().getTime();
     regexp = new RegExp($(this).data('id'), 'g');
     $(this).before($(this).data('fields').replace(regexp, time));
     return event.preventDefault();
   });
+  
 });
