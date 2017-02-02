b05bb4ff9821271df22ca99672c0cf9173163dfc
elton silva <elton.chrls@gmail.com>
2017-02-01 22:07:39 -0200

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 ::Account.create({
   code: 254789,
   username: "homologacao",
-  email: "homologacao@homologacao.com"
+  email: "homologacao@homologacao.com",
   subdomain: "homologacao",
   situation: 0,
   password: "123456789",
