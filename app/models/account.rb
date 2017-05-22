16ed856a80475c5b21064a873c142279aafb09fd
Elton Silva <elton.chrls@gmail.com>
2017-05-22 14:47:00 -0300

    Correção de criação de usuário onde nome é obrigatório

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,7 +3,7 @@ class Account < ApplicationRecord
   # internal engine
   has_many :general_messages,  class_name: "::Internal::GeneralMessage"
   
-  has_many :staffs,   class_name: "::Internal::Staff"
+  has_many :staffs,   class_name: ::Internal::Staff
   has_many :sectors,  class_name: "::Internal::Sector"
   has_many :tasks,    class_name: "::Internal::Task"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,13 @@
-::Account.create({
+@account = ::Account.new({
   code: 254789,
+  name: "homologacao",
   username: "homologacao",
   email: "homologacao@homologacao.com",
   subdomain: "homologacao",
-  situation: 0,
+  situation: 'ativo',
   password: "123456789",
 
 
 })
+
+@account.save!
